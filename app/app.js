45bd27b4da049bfa75925a1d53562e6b5a2c2a16
rysiu21 <bachara27492@student.pwsztar.edu.pl>
2017-05-10 11:52:28 +0200

    1

--- app/app.js
+++ app/app.js
@@ -1,30 +1,30 @@
 
-var joke = "JavaScript walked into a bar....';
-var toldJoke = "false";
-var $punchline = 'Better watch out for those semi-colons.'
-var result
+var joke = 'JavaScript walked into a bar....';
+var toldJoke = 'false';
+var $punchline = 'Better watch out for those semi-colons.';
+//var result;
 
 if (toldJoke === true) {
-    Console.log($punchline);
+    console.log($punchline);
 } else {
     console.log(joke);
 }
 
-var zip_code = 98104;
-var joesFavorite_Movie = Forbidden Planet;
+//var zipCode = 98104;
+var joesFavoriteMovie = 'ForbiddenPlanet';
 var movieTicket$ = 9;
 //
 if (movieTicket$ >= 9) {
-    console.log("Too much!");
+    console.log('Too much!');
 } else {
-    console.log("We're going to see " + joesFavoriteMovie);
+    console.log('We\'re going to see ' + joesFavoriteMovie);
 }
 
 var a = 5;
-var b = "5";
+var b = '5';
 var equal;
 
-if (a == b) {
+if (a === b) {
     equal = true;
 } else {
     equal = false;
